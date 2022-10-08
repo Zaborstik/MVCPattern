9515ecf1a438853cf0d7b6d15e421bc777baa28d
Zaborstik <zaborstik@gmail.com>
2022-10-08 22:13:03 +0300

    part 1

---
+++ src/model/FakeModel.java
@@ -0,0 +1,24 @@
+package src.model;
+
+import src.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model{
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("A", 1l, 1));
+        users.add(new User("B", 2l, 1));
+
+        modelData.setUsers(users);
+    }
+}
---
+++ src/model/Model.java
@@ -0,0 +1,6 @@
+package src.model;
+
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+}
